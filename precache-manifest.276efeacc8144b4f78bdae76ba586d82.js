4156ed5f10f4a66239d779b276e8354303783e56
drsnails <anistu@gmail.com>
2020-10-14 17:16:37 +0300

    Initial dist subtree commit

---
+++ precache-manifest.276efeacc8144b4f78bdae76ba586d82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79ccc125aae10d343ba",
+    "url": "/missBooksVue/css/app.e440cf41.css"
+  },
+  {
+    "revision": "f595030c4b80447d245365ee3d87c493",
+    "url": "/missBooksVue/index.html"
+  },
+  {
+    "revision": "b79ccc125aae10d343ba",
+    "url": "/missBooksVue/js/app.2154c89b.js"
+  },
+  {
+    "revision": "8aa553b58c090ded326c",
+    "url": "/missBooksVue/js/chunk-vendors.34514bf3.js"
+  },
+  {
+    "revision": "8836d98edd5918cc97eae5f80931c809",
+    "url": "/missBooksVue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/missBooksVue/robots.txt"
+  }
+]);
